132685696ba0f07ceb1cc5ee80a8f77b2d1a38cf
Krunoslav Husak <kruno@husak.me>
2023-11-19 19:12:58 +0100

    Easier way to define routes

--- router.go
+++ router.go
@@ -21,3 +21,19 @@ func NewRouter() *Router {
 		ControllerRoutes: make([]ControllerRoute, 0),
 	}
 }
+
+func (r *Router) AddControllerRoute(name string, controller Controller, routes ...Route) {
+	r.ControllerRoutes = append(r.ControllerRoutes, ControllerRoute{
+		Name:       name,
+		Controller: controller,
+		Routes:     routes,
+	})
+}
+
+func NewRoute(method string, path string, handler func(*Context) error) Route {
+	return Route{
+		Method:  method,
+		Path:    path,
+		Handler: handler,
+	}
+}
