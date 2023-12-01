a5760628abf99d2a88ea9fce3471d93afa38870a
Krunoslav Husak <kruno@husak.me>
2023-12-01 10:52:56 +0100

    Wrap context JSON method to include status code

--- context.go
+++ context.go
@@ -2,8 +2,12 @@ package raptor
 
 import "github.com/gofiber/fiber/v2"
 
+type Map map[string]interface{}
+
 type Context struct {
 	*fiber.Ctx
 }
 
-type Map map[string]interface{}
+func (c *Context) JSON(status int, data interface{}) error {
+	return c.Ctx.Status(status).JSON(data)
+}
