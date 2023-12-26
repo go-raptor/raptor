ad7dd6f7755b723642a5356b5e4421a749fedf69
Krunoslav Husak <kruno@husak.me>
2023-12-26 20:51:42 +0100

    Log applying environment variables

--- config.go
+++ config.go
@@ -99,6 +99,7 @@ func (c *Config) applyEnvirontmentVariables() {
 
 func applyEnvirontmentVariable(key string, value interface{}) {
 	if env, ok := os.LookupEnv(key); ok {
+		log.Println("Applying environment variable", key)
 		switch v := value.(type) {
 		case *string:
 			*v = env
