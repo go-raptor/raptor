e072ff010a5fd99cd1152e57e8a228acb2d64251
Krunoslav Husak <kruno@husak.me>
2023-12-31 11:38:16 +0100

    Automatic actions registration

--- controller.go
+++ controller.go
@@ -44,6 +44,7 @@ func (c *Controller) registerActions(functions ...func(*Context) error) {
 	}
 	for _, function := range functions {
 		fullName := runtime.FuncForPC(reflect.ValueOf(function).Pointer()).Name()
+		fmt.Println(fullName)
 		parts := strings.Split(fullName, ".")
 		name := parts[len(parts)-1]
 
@@ -58,19 +59,35 @@ func (c *Controller) registerActions(functions ...func(*Context) error) {
 	}
 }
 
+func (c *Controller) registerAction(name string, function func(*Context) error) {
+	if c.Actions == nil {
+		c.Actions = make(map[string]action)
+	}
+	c.Actions[name] = action{
+		Name:     name,
+		Function: function,
+	}
+}
+
 type Controllers map[string]*Controller
 
-func RegisterController(c interface{}, functions ...func(*Context) error) *Controller {
+func RegisterController(c interface{}) *Controller {
 	val := reflect.ValueOf(c)
 
 	if val.Kind() == reflect.Ptr && val.Elem().FieldByName("Controller").Type() == reflect.TypeOf(Controller{}) {
-		name := val.Elem().Type().Name()
-		if strings.HasSuffix(name, "Controller") {
-			name = strings.TrimSuffix(name, "Controller")
-		}
 		controller := val.Elem().FieldByName("Controller").Addr().Interface().(*Controller)
-		controller.Name = name
-		controller.registerActions(functions...)
+		controller.Name = val.Elem().Type().Name()
+
+		for i := 0; i < val.NumMethod(); i++ {
+			method := val.Method(i)
+			if method.Type().NumIn() == 1 && method.Type().In(0) == reflect.TypeOf(&Context{}) {
+				methodName := val.Type().Method(i).Name
+				if methodName != "Action" {
+					fmt.Println("Method Name:", methodName)
+					controller.registerAction(methodName, method.Interface().(func(*Context) error))
+				}
+			}
+		}
 		return controller
 	} else {
 		panic("Controller must be a pointer to a struct that embeds raptor.Controller")
