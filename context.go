da6737a549910b028f6878de42ba3a8159f7c98c
Krunoslav Husak <kruno@husak.me>
2024-04-18 12:36:01 +0200

    Error handling

--- context.go
+++ context.go
@@ -1,6 +1,9 @@
 package raptor
 
 import (
+	"errors"
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -17,4 +20,16 @@ func (c *Context) JSON(data interface{}, status ...int) error {
 		status = append(status, fiber.StatusOK)
 	}
 	return c.Ctx.Status(status[0]).JSON(data)
+}
+
+func (c *Context) JSONError(err error, status ...int) error {
+	var e *Error
+	if errors.As(err, &e) {
+		return c.Ctx.Status(e.Code).JSON(e)
+	}
+
+	if len(status) == 0 {
+		status = append(status, http.StatusInternalServerError)
+	}
+	return c.Ctx.Status(status[0]).JSON(NewError(status[0], err.Error()))
 }
--- version.go
+++ version.go
@@ -1,5 +1,5 @@
 package raptor
 
 const (
-	Version = "v0.9.0"
+	Version = "v0.9.1"
 )
