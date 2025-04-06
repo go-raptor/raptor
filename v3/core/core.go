375800a20926dc3aeb0b0ce6b1937e96c39f15ce
Krunoslav Husak <kruno@husak.me>
2025-04-06 21:17:17 +0200

    Code cleanup

--- v3/core/core.go
+++ v3/core/core.go
@@ -1,8 +1,12 @@
 package core
 
 import (
+	"log/slog"
+	"time"
+
 	"github.com/go-raptor/components"
 	"github.com/go-raptor/connectors"
+	"github.com/go-raptor/errs"
 )
 
 type Components struct {
@@ -26,4 +30,46 @@ func NewCore(u *components.Utils) *Core {
 		services:    make(map[string]components.ServiceInterface),
 		middlewares: make([]components.MiddlewareInterface, 0),
 	}
+}
+
+func (c *Core) Handle(ctx *components.Context) error {
+	startTime := time.Now()
+
+	h := c.handlers[ctx.Controller][ctx.Action]
+	chain := h.action
+
+	for i := len(c.handlers[ctx.Controller][ctx.Action].middlewares) - 1; i >= 0; i-- {
+		mwIndex := c.handlers[ctx.Controller][ctx.Action].middlewares[i]
+		mw := c.middlewares[mwIndex]
+		currentChain := chain
+		chain = func(ctx *components.Context) error {
+			if err := mw.New(ctx, currentChain); err != nil {
+				if _, ok := err.(*errs.Error); ok {
+					ctx.JSONError(err)
+					return nil
+				}
+				return err
+			}
+			return nil
+		}
+	}
+
+	err := chain(ctx)
+
+	if c.utils.LogLevel.Level() < slog.LevelWarn {
+		c.logAction(ctx, startTime)
+	}
+
+	return err
+}
+
+func (c *Core) logAction(ctx *components.Context, startTime time.Time) {
+	c.utils.Log.Info("Request processed",
+		"ip", ctx.RealIP(),
+		"method", ctx.Request().Method,
+		"path", ctx.Request().URL.Path,
+		"status", ctx.Response().Status,
+		"handler", ActionDescriptor(ctx.Controller, ctx.Action),
+		"duration", time.Since(startTime).Milliseconds(),
+	)
 }
--- v3/core/handler.go
+++ v3/core/handler.go
@@ -1,11 +1,7 @@
 package core
 
 import (
-	"log/slog"
-	"time"
-
 	"github.com/go-raptor/components"
-	"github.com/go-raptor/errs"
 )
 
 type handler struct {
@@ -22,46 +18,4 @@ func newHandler(action func(*components.Context) error) *handler {
 
 func (h *handler) injectMiddleware(middlewareIndex uint8) {
 	h.middlewares = append(h.middlewares, uint8(middlewareIndex))
-}
-
-func (c *Core) Handle(ctx *components.Context) error {
-	startTime := time.Now()
-
-	h := c.handlers[ctx.Controller][ctx.Action]
-	chain := h.action
-
-	for i := len(c.handlers[ctx.Controller][ctx.Action].middlewares) - 1; i >= 0; i-- {
-		mwIndex := c.handlers[ctx.Controller][ctx.Action].middlewares[i]
-		mw := c.middlewares[mwIndex]
-		currentChain := chain
-		chain = func(ctx *components.Context) error {
-			if err := mw.New(ctx, currentChain); err != nil {
-				if _, ok := err.(*errs.Error); ok {
-					ctx.JSONError(err)
-					return nil
-				}
-				return err
-			}
-			return nil
-		}
-	}
-
-	err := chain(ctx)
-
-	if c.utils.LogLevel.Level() < slog.LevelWarn {
-		c.logAction(ctx, startTime)
-	}
-
-	return err
-}
-
-func (c *Core) logAction(ctx *components.Context, startTime time.Time) {
-	c.utils.Log.Info("Request processed",
-		"ip", ctx.RealIP(),
-		"method", ctx.Request().Method,
-		"path", ctx.Request().URL.Path,
-		"status", ctx.Response().Status,
-		"handler", ActionDescriptor(ctx.Controller, ctx.Action),
-		"duration", time.Since(startTime).Milliseconds(),
-	)
 }
